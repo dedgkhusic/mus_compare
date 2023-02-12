6c65ada6ceb1e7ac9a42131df856770a25e34172
ataev <ataev-05@bk.ru>
2023-02-12 18:33:29 +0300

    shit worked!!!

--- vk.py
+++ vk.py
@@ -4,15 +4,17 @@ import time
 
 
 def main():
-    t_start = time.time()
-    user_id1 = '396334047'  # я
-    user_id2 = '550033465'  # алина
+    t_start = time.time()  # начало работы
+    user_id1 = '152062114'  # user1 мама
+    user_id2 = '550033465'  # user2 сёма
 
-    login, password = '+79888384971', 'aRV20050505'
+    # авторизация
+    login, password = '+79891331486', 'AFsdqw12'
     vk_session = vk_api.VkApi(login, password)
     vk_session.auth()
     print('вход')
 
+    # подгрузка всех записей в json
     vkaudio = VkAudio(vk_session)
     tracks1 = vkaudio.get(user_id1)
     print('загруженно user1')
@@ -23,36 +25,33 @@ def main():
     user1 = []
     user2 = []
     for el in tracks1:
-        # user1.append([el['id'], [el['artist'], el['title']]])
-        # print(el['id'], '-', el['artist'], '-', el['title'])
         user1.append(el['id'])
 
     for el in tracks2:
-        # print(el['id'], '-', el['artist'], '-', el['title'])
-        # user2.append([el['id'], [el['artist'], el['title']]])
         user2.append(el['id'])
 
     per = []
-    print(user1)
-    print(user2)
+    # print(user1)
+    # print(user2)
 
     for i in user1:
         if i in user2:
             per.append(i)
 
     print()
-    print(len(per), end='\n')
-    print(f"{(len(per) / len(user1)) * 100}% сопадений user1 c user2")
-    print(f"{(len(per) / len(user2)) * 100}% сопадений user2 c user1", end='\n')
+    print(len(per), "совпадений", end='\n')
+    print(f"{round((len(per) / len(user1)) * 100, 2)}% сопадений user1 c user2")
+    print(f"{round((len(per) / len(user2)) * 100, 2)}% сопадений user2 c user1", end='\n\n')
     # print(per)
 
+    print()
     for el in tracks1:
         if el['id'] in per:
-            print(el['artist'], el['title'])
+            print(f"artist: {el['artist']}  title: {el['title']}")
 
     t_end = time.time()
     print()
-    print('Время выполнения:', (t_end - t_start), 'секунд')
+    print('Время выполнения:', int((t_end - t_start) / 60), 'минут', int(t_end - t_start) % 60, 'секунд')
 
 
 if __name__ == '__main__':
